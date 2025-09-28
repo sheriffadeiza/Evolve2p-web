6d4d090035d06a51b5a428331ba20dce73f318b2
muhammed-jamiu <anatemuhammed@gmail.com>
2025-09-29 00:59:01 +0100

    sign up email page fully responsive

--- app/Signups/Email/page.tsx
+++ app/Signups/Email/page.tsx
@@ -11,15 +11,15 @@ const Page = () => {
       <Header />
 
       {/* Main Content */}
-      <div className="flex flex-1">
+      <div className="flex flex-1 justify-center lg:flex lg:flex-1 lg:justify-star">
         {/* Left Section */}
-        <div className="w-1/2 min-h-[calc(150vh-64px)]">
+        <div className="w-full lg:w-1/2 min-h-[calc(150vh-64px">
           <Ev2pnav />
           <EmailForm />
         </div>
 
         {/* Right Section */}
-        <div className="w-1/2 mt-[20px]  min-h-[calc(150vh-64px)]">
+        <div className="w-full lg:w-1/2 mt-[20px]  min-h-[calc(150vh-64px)] hidden lg:flex ">
           <ImageContent />
         </div>
       </div>
