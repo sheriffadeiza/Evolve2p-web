37ff706e6dea38cdeac3e4c8f974cf8089abb777
sheriffadeiza <sheriffadeiza10@gmail.com>
2025-12-05 21:58:18 +0100

    ll

---
+++ app/notify2/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Notify from '@/components/notify2/notify2'
+
+
+const page = () => {
+  return (
+    <div>
+      <Notify/>
+    </div>
+  )
+}
+
+export default page
